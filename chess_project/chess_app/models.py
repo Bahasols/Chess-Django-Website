ccc247008ad0ec33c267d53b420db906d7faf5ab
Bahasols <hbahaaldin@gmail.com>
2023-11-26 22:31:29 -0500

    Adding models and functions

--- chess_project/chess_app/models.py
+++ chess_project/chess_app/models.py
@@ -1,3 +1,31 @@
+# chess_app/models.py
+
 from django.db import models
 
-# Create your models here.
+class ChessGame(models.Model):
+    # Fields to represent the game state
+    current_player = models.CharField(max_length=10)  # 'white' or 'black'
+    checkmate = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Chess Game {self.id}"
+
+class ChessPiece(models.Model):
+    game = models.ForeignKey(ChessGame, on_delete=models.CASCADE)
+    row = models.IntegerField() # Also known as 'Rank'
+    col = models.IntegerField() # Also known as 'File'
+    color = models.CharField(max_length=10)  # 'white' or 'black'
+    move_count = models.IntegerField()
+    PIECE_TYPES = [
+        ('king', 'King'),
+        ('queen', 'Queen'),
+        ('rook', 'Rook'),
+        ('bishop', 'Bishop'),
+        ('knight', 'Knight'),
+        ('pawn', 'Pawn'),
+    ]
+
+    piece_type = models.CharField(max_length=10, choices=PIECE_TYPES)
+
+    def __str__(self):
+        return f"{self.color.capitalize()} {self.piece_type.capitalize()} at ({self.row}, {self.col})"
---
+++ chess_project/chess_app/utils.py
@@ -0,0 +1,7 @@
+def validate_move(piece,target_row, target_col):
+    #Logic to whether this is a valid move will go here, we still need
+    # to discuss how far we're willing to go with special moves
+    pass
+def is_checkmate(game):
+    #Logic to check whether it is checkmate, i.e are there any possible valid moves that will stop the check.
+    pass
